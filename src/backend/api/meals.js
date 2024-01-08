55fe29fd4d60fe214e922a8bca1babd289c46c68
Aaron Moses <pastures77@hotmail.com>
2024-01-08 14:54:29 +0100

    not done yet!

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -1,14 +1,44 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const knex = require("../database");
+const knex = require('../database');
 
-router.get("/", async (request, response) => {
+router.get('/', async (request, response) => {
   try {
-    // knex syntax for selecting things. Look up the documentation for knex for further info
-    const titles = await knex("meals").select("title");
-    response.json(titles);
+    let query = knex('meal');
+
+    if (request.query.maxPrice) {
+      query = query.where('price', '<', request.query.maxPrice);
+    }
+
+    if (request.query.availableReservations) {
+      query = query.where('max_reservations', '>', 0);
+    }
+
+    if (request.query.title) {
+      query = query.where('title', 'like', `%${request.query.title}%`);
+    }
+
+    if (request.query.dateAfter) {
+      query = query.where('date', '>', request.query.dateAfter);
+    }
+
+    if (request.query.dateBefore) {
+      query = query.where('date', '<', request.query.dateBefore);
+    }
+
+    if (request.query.limit) {
+      query = query.limit(request.query.limit);
+    }
+
+    if (request.query.sortKey) {
+      const direction = request.query.sortDir || 'asc';
+      query = query.orderBy(request.query.sortKey, direction);
+    }
+
+    const meals = await query;
+    response.json(meals);
   } catch (error) {
-    throw error;
+    response.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
