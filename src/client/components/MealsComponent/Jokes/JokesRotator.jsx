f36255cc26935f33f2553ad223e5a0ba84cba1a2
Aaron Moses <pastures77@hotmail.com>
2024-06-06 12:54:46 +0200

    feat: Add new jokes to JokesRotator component

--- src/client/components/MealsComponent/Jokes/JokesRotator.jsx
+++ src/client/components/MealsComponent/Jokes/JokesRotator.jsx
@@ -6,6 +6,8 @@ const JokesRotator = () => {
     'Why don’t eggs tell jokes? They’d crack each other up.',
     'I’m on a seafood diet. I see food and I eat it.',
     'Why did the scarecrow win an award? Because he was outstanding in his field.',
+    'What do you call fake spaghetti? An impasta.',
+    'Why did the coffee file a police report? It got mugged.',
     // Add more jokes as needed
   ];
 
